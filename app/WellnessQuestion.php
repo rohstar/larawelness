e3a17164d7d82b83ff1716d85efa6e576fbe0a2b
Rohan Maheshwari <rohstar@gmail.com>
2017-09-30 12:07:45 -0400

    Estalblishes relationhsips

--- app/WellnessQuestion.php
+++ app/WellnessQuestion.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class WellnessQuestion extends Model
 {
-    //
+
+    protected $guarded = [];
+
+    public function record(){
+
+        return $this->belongsTo(WellnessRecord::class);
+
+    }
+
 }
--- app/WellnessRecord.php
+++ app/WellnessRecord.php
@@ -4,7 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed $user
+ */
 class WellnessRecord extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+
+    }
+
+    public function questions(){
+
+        return $this->hasMany(WellnessQuestion::class);
+
+    }
+
 }
