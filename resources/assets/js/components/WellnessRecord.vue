3a60a0c9dca099cea7a1ddd39699110a0f350d4e
Rohan Maheshwari <rohstar@gmail.com>
2017-10-01 03:05:19 -0400

    updates on WellnessRecord template

--- resources/assets/js/components/WellnessRecord.vue
+++ resources/assets/js/components/WellnessRecord.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
-        <div class="row" style="text-align: center">
-            <question v-for="q in questions" :key="q.id" :ques="q"></question>
+        <div class="row">
+            <question v-for="(item, index) in questions" :key="index" :ques="item" :patient-id="patientId"></question>
         </div>
     </div>
 </template>
@@ -11,39 +11,27 @@
     axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
     export default {
-        data(){
+        data() {
             return {
-                questions: [
-//                    {
-//                        id: 1,
-//                        question: 'hello?',
-//                        option_1: 1,
-//                        option_2: 2,
-//
-//                    },{
-//                        id: 2,
-//                        question: 'hello again?',
-//                        option_1: 2,
-//                        option_2: 3,
-//
-//                    },
-                ]
+                questions: []
             }
         },
-        props:[
+        props: [
             'patientId'
         ],
-        created(){
-            console.log(this.patientId);
-
+        created() {
             this.fetchQuestionsForPatient(this.patientId);
         },
-        methods:{
+        methods: {
 
-            fetchQuestionsForPatient(id){
+            fetchQuestionsForPatient(id) {
 
-                axios.get('/api/user/'+id+'/questions/today')
-                    .then(response => console.log(response.data))
+                let vm = this;
+                axios.get('/api/user/' + id + '/questions/today')
+                    .then(function (response) {
+
+                        vm.questions = response.data
+                    })
                     .catch(response => console.log(response.data));
 
             }
