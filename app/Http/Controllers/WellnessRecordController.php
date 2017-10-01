20f4270c9cd93aede0fb27c2d1455446f3294dbb
Rohan Maheshwari <rohstar@gmail.com>
2017-10-01 03:05:59 -0400

    adds wellness record create

--- app/Http/Controllers/WellnessRecordController.php
+++ app/Http/Controllers/WellnessRecordController.php
@@ -22,9 +22,10 @@ class WellnessRecordController extends Controller
 
         foreach (WellnessQuestion::all() as $q){
 
-            $data[$q->id]['question'] = $q;
+            $data[$q->id]['info'] = $q;
 
             if(isset($answered[$q->id])){
+                $data[$q->id]['answer'] = $answered[$q->id];
                 $data[$q->id]['answer'] = $answered[$q->id];
             } else {
                 $data[$q->id]['answer'] = null;
@@ -35,6 +36,52 @@ class WellnessRecordController extends Controller
 
         //returns all questions for the day and whether they were answered.
         return $data;
+
+    }
+
+    public function create(Request $request){
+
+        $user =  User::find($request->input('user_id'));
+        $date = Carbon::now()->toDateString();
+
+        $answer_key = $request->input('answer_key');
+        $question_id = $request->input('question_id');
+
+        //determine if a daily record exists
+        $record = $user->records()->where('date', $date)->first();
+
+        if($record->exists()){
+
+            //determine if question has been answered for today's record
+            $user_answer = $record->questions()
+                ->where('wellness_question_id', $question_id)->first();
+
+            if($user_answer == null){
+
+                //if not, make a many to many column on user_records
+                $record->questions()->attach($question_id, ['answer_key' => $answer_key]);
+                dd();
+
+            } else {
+
+                $user_answer->pivot->answer_key = $answer_key;
+                $user_answer->pivot->save();
+
+            };
+
+
+        } else {
+
+            $record = WellnessRecord::create([
+
+                'user_id' => $user->id,
+                'date' => $date
+
+            ]);
+
+        };
+
+
 
     }
 
