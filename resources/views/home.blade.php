8a1398265d191e701a6a3dfbe3b753077cd3785f
Rohan Maheshwari <rohstar@gmail.com>
2017-10-01 00:46:23 -0400

    modifies to show components on home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
+                <div class="panel-heading">Hello, {{auth()->user()->name}}</div>
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <record :patient-id="{{auth()->user()->id}}"></record>
                 </div>
             </div>
         </div>
