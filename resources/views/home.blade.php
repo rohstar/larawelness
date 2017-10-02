e63f4c9148d2b30685527cf5c190a9c66bb7afa2
Rohan Maheshwari <rohstar@gmail.com>
2017-10-02 12:32:54 -0400

    header replace

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="page-header">
-                    <h1 style="font-size: 45px">Wellness Questions {{\Carbon\Carbon::now()->toDateString()}}
+                    <h1 style="font-size: 45px">Today's Wellness Questions
                     </h1>
                 </div>
                 <record :patient-id="{{auth()->user()->id}}" :record="{{$record}}"></record>
