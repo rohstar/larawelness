b7adfb94545802969eb479968b53c28754e98c93
Rohan Maheshwari <rohstar@gmail.com>
2017-10-02 12:17:06 -0400

    text

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container">
         <div class="page-header">
-            <h1 style="font-size: 45px">Wellness History (past week)}
-            </h1>
+            <h4 style="font-size: 30px">Wellness History (past week)
+            </h4>
         </div>
         <history></history>
     </div>
